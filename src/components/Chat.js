07b59ffa7d16d659cfeb0ad2b3953af4d32c3fb8
matthewraymondh <raymondhartono76@gmail.com>
2022-01-15 23:43:20 +0700

    adding chat functionality

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,20 +2,62 @@ import { HashtagIcon } from "@heroicons/react/outline";
 import {
   BellIcon,
   ChatIcon,
+  EmojiHappyIcon,
+  GiftIcon,
   InboxIcon,
+  PlusCircleIcon,
   QuestionMarkCircleIcon,
   SearchIcon,
   UsersIcon,
 } from "@heroicons/react/solid";
+import { useRef } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useSelector } from "react-redux";
 import { selectChannelId, selectChannelName } from "../features/channelSlice";
-import { auth } from "../firebase";
+import { auth, db } from "../firebase";
+import firebase from "firebase";
+import { useCollection } from "react-firebase-hooks/firestore";
+import Message from "./Message";
 
 function Chat() {
   const channelId = useSelector(selectChannelId);
   const channelName = useSelector(selectChannelName);
   const [user] = useAuthState(auth);
+  const inputRef = useRef("");
+  const chatRef = useRef(null);
+  const [messages] = useCollection(
+    channelId &&
+      db
+        .collection("channels")
+        .doc(channelId)
+        .collection("messages")
+        .orderBy("timestamp", "asc")
+  );
+
+  const scrollToBottom = () => {
+    chatRef.current.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  };
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+
+    if (inputRef.current.value !== "") {
+      db.collection("channels").doc(channelId).collection("messages").add({
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        message: inputRef.current.value,
+        name: user?.displayName,
+        photoURL: user?.photoURL,
+        email: user?.email,
+      });
+    }
+
+    inputRef.current.value = "";
+    scrollToBottom();
+  };
+
   return (
     <div className="flex flex-col h-screen">
       <header className="flex items-center justify-between space-x-5 border-b border-gray-800 p-4 -mt-1">
@@ -39,6 +81,42 @@ function Chat() {
           <QuestionMarkCircleIcon className="icon" />
         </div>
       </header>
+      <main className="flex-grow overflow-y-scroll scrollbar-hide">
+        {messages?.docs.map((doc) => {
+          const { message, timestamp, name, photoURL, email } = doc.data();
+          return (
+            <Message
+              key={doc.id}
+              id={doc.id}
+              message={message}
+              timestamp={timestamp}
+              name={name}
+              email={email}
+              photoURL={photoURL}
+            />
+          );
+        })}
+        <div ref={chatRef} className="pb-16" />
+      </main>
+      <div className="flex items-center p-2.5 bg-[#40444b] mx-5 mb-7 rounded-lg">
+        <PlusCircleIcon className="icon mr-4" />
+        <form className="flex-grow">
+          <input
+            type="text"
+            disabled={!channelId}
+            placeholder={
+              channelId ? `Message #${channelName}` : "Select a channel"
+            }
+            className="bg-transparent focus:outline-none text-[#dcddde] w-full placeholder-[#72767d] text-sm"
+            ref={inputRef}
+          />
+          <button hidden type="submit" onClick={sendMessage}>
+            Send
+          </button>
+        </form>
+        <GiftIcon className="icon mr-2" />
+        <EmojiHappyIcon className="icon" />
+      </div>
     </div>
   );
 }
